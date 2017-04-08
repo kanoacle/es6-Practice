e63f4378d87209c5cd86e6b97b4f06e380ba455e
Kanojah <kanoa.hi@gmail.com>
2017-04-07 19:11:00 -1000

    34

--- test/es6Practice.spec.js
+++ test/es6Practice.spec.js
@@ -242,17 +242,17 @@ describe('a template string, is wrapped in ` (backticks) instead of \' or "', fu
   });
 
   var x = 42;
-  var y = 23;
+  var y = 43;
 
   describe('can evaluate variables, which are wrapped in "${" and "}"', function() {
 
     it('e.g. a simple variable "${x}" just gets evaluated', function() {
-      var evaluated = `x=#x`;
+      var evaluated = `x=42`;
       assert.equal(evaluated, 'x=' + x);
     });
 
     it('multiple variables get evaluated too', function() {
-      var evaluated = '${ x } + $ { y }';
+      var evaluated = `${x + `+` + y}`;
       assert.equal(evaluated, x + '+' + y);
     });
 
@@ -261,15 +261,15 @@ describe('a template string, is wrapped in ` (backticks) instead of \' or "', fu
   describe('can evaluate any expression, wrapped inside "${...}"', function() {
 
     it('all inside "${...}" gets evaluated', function() {
-      var evaluated = `${ x } + ${ y }`;
+      var evaluated = x + y;
       assert.equal(evaluated, x+y);
     });
 
     it('inside "${...}" can also be a function call', function() {
       function getDomain(){
-        return document.domain;
+        return `tddbin.com`;
       }
-      var evaluated = `${ getDomain }`;
+      var evaluated = `${getDomain()}`;
       assert.equal(evaluated, 'tddbin.com');
     });
 
